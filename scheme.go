fabd9a6640331787a7b6764d9e202e1be49af884
Simon Tucker <brotherlogic@gmail.com>
2020-10-30 16:00:46 -0700

    Adds keeper width scheme. This closes #7.

--- scheme.go
+++ scheme.go
@@ -1,9 +1,20 @@
 package main
 
-import pbrc "github.com/brotherlogic/recordcollection/proto"
+import rcpb "github.com/brotherlogic/recordcollection/proto"
 
 type schemeGenerator interface {
 	// Does this record apply to the this filter
-	filter(rec *pbrc.Record) bool
+	filter(rec *rcpb.Record) (bool, bool)
 	name() string
+}
+
+type keeperScheme struct{}
+
+func (ks *keeperScheme) filter(rec *rcpb.Record) (bool, bool) {
+	//Is it a keeper?, doess it have a width?
+	return rec.GetMetadata().GetGoalFolder() == 466902, rec.GetMetadata().GetRecordWidth() > 0
+}
+
+func (ks *keeperScheme) name() string {
+	return "keeper_width"
 }
