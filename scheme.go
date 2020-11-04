59bcf0e8df2e16066e41bb54265ee8a739bd6325
Simon Tucker <brotherlogic@gmail.com>
2020-11-04 07:29:19 -0800

    Include cd width here

--- scheme.go
+++ scheme.go
@@ -18,3 +18,14 @@ func (ks *keeperScheme) filter(rec *rcpb.Record) (bool, bool) {
 func (ks *keeperScheme) name() string {
 	return "keeper_width"
 }
+
+type cdScheme struct{}
+
+func (cds *cdScheme) filter(rec *rcpb.Record) (bool, bool) {
+	//Is it a keeper?, doess it have a width?
+	return rec.GetMetadata().GetGoalFolder() == 242018, rec.GetMetadata().GetRecordWidth() > 0 && rec.GetMetadata().GetCategory() != rcpb.ReleaseMetadata_PRE_VALIDATE
+}
+
+func (cds *cdScheme) name() string {
+	return "cd_width"
+}
