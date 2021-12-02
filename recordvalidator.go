11429b5ebf62ddd1f9fd9503c6e6bf203a2ccb52
Simon Tucker <brotherlogic@gmail.com>
2021-12-02 05:53:41 -0800

    log details

--- recordvalidator.go
+++ recordvalidator.go
@@ -107,6 +107,7 @@ func (s *Server) updateMetrics(schemes *pb.Schemes) {
 		togo := float64(len(sc.GetInstanceIds()) - len(sc.GetCompletedIds()))
 		days := togo / compPerDay
 		ftime := time.Now().Add(time.Hour * time.Duration(24*days))
+		log.Printf("COMP %v, %v, %v, %v", compPerDay, togo, days, ftime)
 		perDay.With(prometheus.Labels{"scheme": sc.GetName()}).Set(float64(compPerDay))
 		completionDateV2.With(prometheus.Labels{"scheme": sc.GetName()}).Set(float64(ftime.Unix()))
 	}
