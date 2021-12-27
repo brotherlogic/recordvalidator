b8143a0a5cc1975195abe107a9bcd7bdfe5a498b
Simon Tucker <brotherlogic@gmail.com>
2021-12-27 11:12:01 -0800

    Fix scheme for sleeves

--- scheme.go
+++ scheme.go
@@ -152,7 +152,7 @@ type nsSleeve struct{}
 
 func (nss *nsSleeve) filter(rec *rcpb.Record) (bool, bool, float32) {
 	return rec.GetMetadata().GetFiledUnder() == rcpb.ReleaseMetadata_FILE_12_INCH && rec.GetRelease().GetFolderId() == 242017,
-		rec.GetMetadata().GetSleeve() == rcpb.ReleaseMetadata_VINYL_STORAGE_DOUBLE_FLAP, -1
+		rec.GetMetadata().GetSleeve() != rcpb.ReleaseMetadata_SLEEVE_UNKNOWN, -1
 }
 
 func (nss *nsSleeve) name() string {
@@ -163,7 +163,7 @@ type nsSevenSleeve struct{}
 
 func (nss *nsSevenSleeve) filter(rec *rcpb.Record) (bool, bool, float32) {
 	return rec.GetMetadata().GetFiledUnder() == rcpb.ReleaseMetadata_FILE_7_INCH && rec.GetRelease().GetFolderId() == 267116,
-		rec.GetMetadata().GetSleeve() == rcpb.ReleaseMetadata_VINYL_STORAGE_DOUBLE_FLAP, -1
+		rec.GetMetadata().GetSleeve() != rcpb.ReleaseMetadata_SLEEVE_UNKNOWN, -1
 }
 
 func (nss *nsSevenSleeve) name() string {
