4d8557bc35dec4c3cbd857242fbaffffa4ad5983
Simon Tucker <brotherlogic@gmail.com>
2022-11-27 14:31:22 -0800

    Fix for no_dig

--- scheme.go
+++ scheme.go
@@ -279,7 +279,7 @@ func (os *olderND) name() string {
 }
 
 func (os *olderND) filter(rec *rcpb.Record) (bool, bool, float32) {
-	return rec.GetRelease().GetFormatQuantity() == 1 &&
+	return rightFormatQuantity(rec) &&
 			rec.GetMetadata().GetCategory() != rcpb.ReleaseMetadata_UNKNOWN &&
 			rec.GetMetadata().GetCategory() != rcpb.ReleaseMetadata_ARRIVED &&
 			rec.GetMetadata().GetCategory() != rcpb.ReleaseMetadata_PARENTS &&
@@ -376,7 +376,7 @@ func (os *newerND) name() string {
 }
 
 func (os *newerND) filter(rec *rcpb.Record) (bool, bool, float32) {
-	return rec.GetRelease().GetFormatQuantity() == 1 &&
+	return rightFormatQuantity(rec) &&
 			rec.GetMetadata().GetCategory() != rcpb.ReleaseMetadata_UNKNOWN &&
 			rec.GetMetadata().GetCategory() != rcpb.ReleaseMetadata_ARRIVED &&
 			rec.GetMetadata().GetCategory() != rcpb.ReleaseMetadata_PARENTS &&
