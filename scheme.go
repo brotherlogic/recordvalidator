e3616cc0f90fc11a8ee00109057ce7f3f65b89e9
Simon Tucker <brotherlogic@gmail.com>
2023-03-01 16:37:09 -0800

    Ensure we include boxed records

--- scheme.go
+++ scheme.go
@@ -9,7 +9,7 @@ import (
 )
 
 func acceptable(r *rcpb.Record) bool {
-	return r.GetMetadata().GetFiledUnder() == rcpb.ReleaseMetadata_FILE_12_INCH
+	return r.GetMetadata().GetFiledUnder() == rcpb.ReleaseMetadata_FILE_12_INCH || r.GetMetadata().GetFiledUnder() == rcpb.ReleaseMetadata_FILE_UNKNOWN
 }
 
 func rightFormatQuantity(r *rcpb.Record) bool {
