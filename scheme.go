aab38ddc2b94d4c5b288f75ce1e83ffc09134c22
Simon Tucker <brotherlogic@gmail.com>
2023-03-04 12:00:09 -0800

    Fix test

--- scheme.go
+++ scheme.go
@@ -711,7 +711,7 @@ func (*fastDump) name() string {
 }
 
 func (*fastDump) filter(rec *rcpb.Record) (bool, bool, float32) {
-	return rec.GetMetadata().GetGoalFolder() == 24107,
+	return rec.GetMetadata().GetGoalFolder() == 24107 && rec.GetMetadata().GetBoxState() == rcpb.ReleaseMetadata_IN_THE_BOX,
 		rec.GetMetadata().GetBoxState() != rcpb.ReleaseMetadata_OUT_OF_BOX,
 		float32(rec.GetMetadata().GetLastListenTime())
 
