d3b949e01c0d44771deefd49f0b5b62f46b7ef51
Simon Tucker <brotherlogic@gmail.com>
2023-09-29 11:28:17 -0700

    Updates keepers when we're done

--- recordvalidatorapi.go
+++ recordvalidatorapi.go
@@ -36,11 +36,14 @@ func (s *Server) ClientUpdate(ctx context.Context, in *rcpb.ClientUpdateRequest)
 			doneParents = true
 		}
 	}
-
 	for _, scheme := range schemes.GetSchemes() {
 		if scheme.GetName() == "keepers" || scheme.GetName() == "keepers_single" {
 			scheme.Active = doneParents
 		}
+	}
+	err = s.save(ctx, schemes)
+	if err != nil {
+		return nil, err
 	}
 
 	// Don't validate records until they've arrived
